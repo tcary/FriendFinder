d22bae389dcda3f49b88eb9bd4cf5c62c4195ae2
Tetiana Cary <taniushka.tata@gmail.com>
2019-10-13 21:59:17 -0600

    Added an array of best friends matches

---
+++ app/data/friends.js
@@ -0,0 +1,72 @@
+// Array holds data objects, a list of all friends and applicable data
+module.exports = [
+    {
+        name: "Henry",
+        photo: "https://www.askideas.com/media/10/Duck-Face-Girl-Funny-People.jpg",
+        answers: [3, 1, 1, 1, 5, 4, 1, 4, 4, 5]
+    },
+    {
+        name: "Melodie",
+        photo: "https://i.guim.co.uk/img/media/8c4ec0ed4a683944bc76bf0ff5580d60d6319b77/0_921_4594_2756/master/4594.jpg?width=300&quality=85&auto=format&fit=max&s=7fd0ab215c3d2dc6ae6be13b26eeacc6",
+        answers: [5, 4, 5, 1, 1, 5, 1, 5, 5, 5]
+    },
+    {
+        name: "Jim Bim",
+        photo: "https://halterman2013infsinlab.files.wordpress.com/2013/01/funnyp2.jpg",
+        answers: [4, 1, 2, 1, 5, 5, 1, 5, 5, 5]
+    },
+    {
+        name: "Twinkie",
+        photo: "https://www.bestfunnies.com/wp-content/uploads/2017/03/Funny-People-make-a-Funny-01.jpg",
+        answers: [5, 1, 4, 1, 5, 5, 1, 5, 5, 5]
+    },
+    {
+        name: "Peter",
+        photo: "https://s3.amazonaws.com/tinycards/image/b839adf86d013ee44871435fd7f9a048",
+        answers: [5, 1, 3, 4, 5, 5, 1, 4, 3, 5]
+    },
+    {
+        name: "Snowball",
+        photo: "https://buzzerg.com/wp-content/uploads/8589130509616-funny-people-wallpaper-hd.jpg",
+        answers: [4, 2, 4, 4, 5, 4, 1, 5, 5, 5]
+    },
+    {
+        name: "Sexy Mama",
+        photo: "https://www.bestfunnies.com/wp-content/uploads/2015/05/Funny-Looking-Wired-People-20.jpg",
+        answers: [4, 1, 1, 1, 3, 1, 1, 5, 2, 5]
+    },
+    {
+        name: "Olenna",
+        photo: "https://nationalpostcom.files.wordpress.com/2011/10/debra.gif",
+        answers: [5, 1, 2, 5, 5, 5, 1, 3, 4, 5]
+    },
+    {
+        name: "Donald",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfXkpIbXWkPunfjMLO_xvbmw3eOvQ7_nuBzDHEL7A4IqKXudrZ",
+        answers: [5, 1, 5, 1, 5, 3, 1, 4, 4, 5]
+    },
+    {
+        name: "SweetPea",
+        photo: "http://funhuh.com/wp-content/uploads/2014/06/01975_0271.jpg",
+        answers: [5, 5, 5, 3, 1, 3, 1, 5, 5, 5]
+    },
+    {
+        name: "Cookie Monster",
+        photo: "https://www.askideas.com/media/10/Fat-Girl-Try-To-Eat-Funny-People.jpg",
+        answers: [5, 3, 5, 5, 5, 5, 1, 4, 4, 5]
+    },
+    {
+        name: "Unicorn",
+        photo: "http://www.funnyandhappy.com/wp-content/uploads/2014/f/funny-people-on-facebook-and-the-internet-4.jpg",
+        answers: [5, 1, 4, 1, 5, 1, 1, 4, 2, 5]
+    },
+    {
+        name: "Jaime",
+        photo: "https://cdn.lifehack.org/wp-content/uploads/2015/12/11053816/funny-grimace-388987_960_720.jpg",
+        answers: [3, 1, 4, 3, 5, 5, 1, 4, 3, 5]
+    }
+];
+
+// module.exports.push2arr = function(data) {
+// 	module.exports.allFriends.push(data);
+// }
